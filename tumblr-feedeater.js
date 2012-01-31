a967ceda7e5ac525aa650bb22d64505683494161
pete gamache <pete@where.com>
2012-01-30 20:22:16 -0500

    improvementz

--- tumblr-feedeater.js
+++ tumblr-feedeater.js
@@ -3,11 +3,11 @@
 // january 2012   pete gamache   gamache!#$@!#gmail.com
 
 FEEDEATER = {
-  // config variables -- set them directly
-  tumblr_host: null,
-  image_width: 500,
+  // config variables -- you can set them directly
+  tumblr_host: null,  // e.g., 'omgblogz.tumblr.com'
+  image_width: 500,   // options: 75, 100, 250, 400, 500, 1280
 
-  // ... or use the set() function to set multiple options
+  // ... or use the set({...}) function to set a hash of options
   set: function (opts) {
     for (var key in opts) {
       if (opts.hasOwnProperty(key)) {
@@ -16,7 +16,7 @@ FEEDEATER = {
     }
   },
 
-  // render_all_posts renders all posts (duh) into the #tumblr-posts element
+  // render_posts renders all posts into the #tumblr-posts element
   // or the value of opts['target'] if specified
   render_posts: function (opts) {
     if (!FEEDEATER.tumblr_host) {
@@ -29,37 +29,36 @@ FEEDEATER = {
     for (var i in tumblr_api_read['posts']) {
       var post = tumblr_api_read['posts'][i];
       target.append(
-        FEEDEATER.render(post)
-          .append(FEEDEATER.render_date(post))
+        FEEDEATER.render(post).append(FEEDEATER.render_date(post))
       );
     }
   },
 
-  // render[post.type](post) returns a jQuery rendering of a post
+  // render(post) returns a jQuery rendering of a post
   render: function (post) {
     return FEEDEATER.renderers[post.type](post);
   },
   renderers: {
     regular: function (post) {
-      return $('<div class="tumblr-post '+post.type+'"></div>')
+      return FEEDEATER.render_div(post)
                .html('<h2 class="title">' + post['regular-title'] + '</h2>' +
                      '<div class="regular-body">' + post['regular-body'] +
                      '</div>');
     },
     link: function (post) {
-      return $('<div class="tumblr-post '+post.type+'"></div>')
+      return FEEDEATER.render_div(post)
                .html('<a href="' + post['link-url'] + '">' +
                      post['link-text'] + '</a>');
     },
     quote: function (post) {
-      return $('<div class="tumblr-post '+post.type+'"></div>')
+      return FEEDEATER.render_div(post)
                .html('<dd>' + post['quote-text'] + '</dd>' +
                      '<dt>' + post['quote-source'] + '</dt>');
     },
     photo: function (post) {
-      return $('<div class="tumblr-post '+post.type+'"></div>')
-               .html('<img src="' + post['photo-url-500'] + '">' +
-                     '<div class="caption">' + post['photo-caption'] +
+      return FEEDEATER.render_div(post)
+               .html('<img src="' + post['photo-url-' + FEEDEATER.image_width]
+                     + '">' + '<div class="caption">' + post['photo-caption'] +
                      '</div>');
     },
     conversation: function (post) {
@@ -70,25 +69,30 @@ FEEDEATER = {
                      line['line-name'] + '</span><span class="text">' +
                      line['line-text'] + '</span></div>');
       }
-      return $('<div class="tumblr-post '+post.type+'"></div>')
+      return FEEDEATER.render_div(post)
                .html('<h2>' + post['conversation-title'] + '</h2>')
                .append(convo);
     },
     video: function (post) {
-      return $('<div class="tumblr-post '+post.type+'"></div>')
+      return FEEDEATER.render_div(post)
                .html('<h2>' + post['video-caption'] + '</h2>' +
                      post['video-player']);
     },
     audio: function (post) {
-      return $('<div class="tumblr-post '+post.type+'"></div>')
+      return FEEDEATER.render_div(post)
                .html('<h2>' + post['audio-caption'] + '</h2>' +
                      post['audio-player']);
     },
     answer: function (post) {
-      return $('<div class="tumblr-post '+post.type+'"></div>')
+      return FEEDEATER.render_div(post)
                .html('<div class="question">' + post['question'] + '</div>' +
                      '<div class="answer">' + post['answer'] + '</div>');
     }
+  },
+
+  // render_div(post) returns an empty jQuery post div
+  render_div: function (post) {
+    return $('<div class="tumblr-post '+post.type+'"></div>');
   },
 
   // render_date(post) returns a jQuery rendering of a post's date
